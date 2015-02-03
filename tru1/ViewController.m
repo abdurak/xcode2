920b01da192f2ca038c33145fd6a9ff18ee01aca
Abdurrahman Akkas <akkas.a@ic10ddb1bff4ab.nunet.neu.edu>
2015-02-03 15:17:40 -0500

    2

--- tru1/ViewController.m
+++ tru1/ViewController.m
@@ -13,10 +13,16 @@
 @end
 
 @implementation ViewController
-
+UIButton * b;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+   b =[UIButton buttonWithType:UIButtonTypeRoundedRect];
+    b.frame=CGRectMake(100,100,100,30);
+    [b setTitle:@"hey" forState:UIControlStateNormal];
+    [b addTarget:self action:@selector(onButton:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:b];
+    NSLog(@"hey");
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,6 +30,10 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)onButton:(id)sender{
+    b.frame=CGRectMake(100,100,b.bounds.size.width+200,30);
 }
 
 @end
